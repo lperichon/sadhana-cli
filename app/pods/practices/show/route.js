eee9156bbc2ff0fc99d72a448e9b1a4aec87352a
Luis Perichon <luisperichon@gmail.com>
2015-03-25 17:17:58 -0300

    reload practice on show route

--- app/pods/practices/show/route.js
+++ app/pods/practices/show/route.js
@@ -5,5 +5,9 @@ import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixi
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function(params) {
     return this.store.find('practice', params.id)
+  },
+  setupController: function(controller, model) {
+    model.reload();
+    controller.set("model", model);
   }
 });

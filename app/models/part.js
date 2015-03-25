63c12975f4840ea745f141bd4320db6f7bf3a284
Luis Perichon <luisperichon@gmail.com>
2015-03-25 17:17:08 -0300

    complete part fixture

--- app/models/part.js
+++ app/models/part.js
@@ -9,6 +9,12 @@ export default Part;
 Part.reopenClass({
   FIXTURES: [
     { id: 1, symbol: 'mudra' },
-    { id: 2, symbol: 'puja' }
+    { id: 2, symbol: 'puja' },
+    { id: 3, symbol: 'mantra'},
+    { id: 4, symbol: 'pranayama'},
+    { id: 5, symbol: 'kriya'},
+    { id: 6, symbol: 'asana'},
+    { id: 7, symbol: 'yoganidra'},
+    { id: 8, symbol: 'samyama'}
   ]
 });

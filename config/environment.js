3b8948b7b65d4d029465307b4043fab04d6abde0
Luis Perichon <luisperichon@gmail.com>
2015-04-12 13:03:30 -0300

    set up cordova app

--- config/environment.js
+++ config/environment.js
@@ -3,10 +3,9 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'sadhana-cli',
-    podModulePrefix: 'sadhana-cli/pods',
     environment: environment,
     baseURL: '/',
-    locationType: 'auto',
+    defaultLocationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -17,16 +16,20 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    cordova: {
+      rebuildOnChange: false,
+      emulate: false
     }
   };
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.APP.API_HOST = 'http://localhost:3000'
+    ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
@@ -43,23 +46,6 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
 
-  }
-
-  ENV['simple-auth-devise'] = {
-    resourceName: 'user',
-    identificationAttributeName: 'email',
-    serverTokenEndpoint: ENV.APP.API_HOST + '/users/sign_in'
-  };
-
-  ENV['simple-auth'] = {
-    crossOriginWhitelist: [ENV.APP.API_HOST],
-    authorizer: 'simple-auth-authorizer:devise',
-    routeAfterAuthentication: 'practices'
-  }
-
-  ENV.contentSecurityPolicy = {
-    'connect-src': "'self' " + ENV.APP.API_HOST,
-    'img-src': "'self' http://s3.amazonaws.com"
   }
 
   return ENV;

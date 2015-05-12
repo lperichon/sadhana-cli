3c60f96c00dfddd81dc17713b4ba4de3756e610a
Luis Perichon <luisperichon@gmail.com>
2015-05-12 13:32:39 -0300

    play chime on android

--- app/pods/components/countdown-timer/component.js
+++ app/pods/components/countdown-timer/component.js
@@ -15,9 +15,22 @@ export default Ember.Component.extend({
         self.$('#clock').val(current_time);
       },
       complete: function() {
+        self.playSound("chime");
         self.sendAction();
       }
     }));
+  },
+  playSound: function(id) {
+    var audioElement =  self.$('#' + id);
+    var url = audioElement.attr('src');
+    var my_media = new Media(url,
+      // success callback
+       function () { console.log("playAudio():Audio Success"); },
+      // error callback
+       function (err) { console.log("playAudio():Audio Error: " + err); }
+    );
+    // Play audio
+    my_media.play();
   },
   actions: {
     start: function() {

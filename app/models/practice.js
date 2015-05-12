305b6db6936609a278bcf92f61c865c2da52249f
Luis Perichon <luisperichon@gmail.com>
2015-05-12 18:42:31 -0300

    add continuous & delay attributes to practice

--- app/models/practice.js
+++ app/models/practice.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
-  practiceParts: DS.hasMany('practice-part')
+  practiceParts: DS.hasMany('practice-part'),
+  continuous: DS.attr('boolean'),
+  delay: DS.attr('number')
 });
